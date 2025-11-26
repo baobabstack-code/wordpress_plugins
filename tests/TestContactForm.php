a4a0451002e8156c1a72d7c486c1c4c482d43173
Baobab Stack <baobabstack@gmail.com>
2025-11-26 14:52:34 +0200

    Revert: remove tests/TestContactForm.php (added earlier)

--- tests/TestContactForm.php
+++
@@ -1,8 +0,0 @@
-<?php
-use PHPUnit\Framework\TestCase;
-
-class TestContactForm extends TestCase {
-    public function test_plugin_file_exists() {
-        $this->assertFileExists(__DIR__ . '/../simple-frontend-plugin.php');
-    }
-}
