6a21d09ce9ea260f3b21ab04eae3b9192c9ac596
Nyasha Shepard Ushewokunze <baobabstack@gmail.com>
2025-12-06 12:47:19 +0200

    Delete baobabstack-custom-assets directory

--- baobabstack-custom-assets/assets/js/admin.js
+++
@@ -1,2 +0,0 @@
-// Admin-only JS: add your custom code here.
-console.log('Baobabstack Custom Assets: admin script loaded');
--- baobabstack-custom-assets/assets/js/custom.js
+++
@@ -1,2 +0,0 @@
-// Public-facing JS: add your custom code here.
-console.log('Baobabstack Custom Assets: public script loaded');
